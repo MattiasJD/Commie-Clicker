da1bebfb24ededbe84027929cd9fe3acb009c13c
MattiasJD <maty.demjan@seznam.cz>
2021-09-19 00:14:38 +0200

    mps, lepsi gui

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ let cun1 = document.getElementById("cun1");
 let cun2 = document.getElementById("cun2");
 let cu1 = 0;
 let cu2 = 0;
+let mps = document.getElementById("mps");
+let mp = 0;
 const d = () => {
   money = money + 1000;
   counter.innerText = money + " devizových korun";
@@ -19,6 +21,8 @@ const buyAutoOne = () => {
         money = money - 100000;
         cu1 = cu1 + 1;
         cun1.innerText = "Vlastněno " + cu1 + "x";
+        mp = mp+500;
+        mps.innerText = mp + " devizových korun za sekundu"
         Auto = setInterval(() => {
             money = money + 500;
             counter.innerText = money + " devizových korun";
@@ -31,6 +35,8 @@ const buyAutoTwo = () => {
         money = money - 200000;
         cu2++;
         cun2.innerText = "Vlastněno " + cu2 + "x";
+        mp = mp+1250;
+        mps.innerText = mp + " devizových korun za sekundu"
         Auto2 = setInterval(() => {
             money = money + 1250;
             counter.innerText = money + " devizových korun";
