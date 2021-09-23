24e3be4c44ab8311e4b618dec6fc0b364644bc11
MattiasJD <maty.demjan@seznam.cz>
2021-09-23 20:50:42 +0200

    v0.4

--- script.js
+++ script.js
@@ -3,28 +3,43 @@ let btn = document.getElementById("btn");
 let counter = document.getElementById("counter");
 let autoOne = document.getElementById("autoOne");
 let autoTwo = document.getElementById("autoTwo");
-let autoThree = document.getElementById("autoThree")
+let autoThree = document.getElementById("autoThree");
+let betterMouse1 = document.getElementById("betterMouse1");
 let cun1 = document.getElementById("cun1");
 let cun2 = document.getElementById("cun2");
-let cun3 = document.getElementById("cun3")
+let cun3 = document.getElementById("cun3");
+let cun4 = document.getElementById("cun4");
 let cu1 = 0;
 let cu2 = 0;
 let cu3 = 0;
+let cu4 = 0;
 let mps = document.getElementById("mps");
 let mp = 0;
 const d = () => {
   money = money + 1000;
   counter.innerText = money + " devizových korun";
 }
-btn.onclick = d;
 let Auto;
 let Auto2;
 let Auto3;
+const buyBetterMouse1 = () => {
+    if(money >= 268000){
+        money = money - 268000;
+        cu4++;
+        cun4.innerText = "Vlastneno " + cu4 + "x";
+        const dd = () => {
+            money = money + 4000;
+            counter.innerText = money + " devizových korun";
+        }
+    }
+}
+btn.onclick = d;
+betterMouse1.onclick = buyBetterMouse1;
 const buyAutoOne = () => {
     if(money >= 100000){
         money = money - 100000;
         cu1 = cu1 + 1;
-        cun1.innerText = "Vlastněno " + cu1 + "x";
+        cun1.innerText = "Vlastneno " + cu1 + "x";
         mp = mp+500;
         mps.innerText = mp + " devizových korun za sekundu"
         Auto = setInterval(() => {
@@ -38,7 +53,7 @@ const buyAutoTwo = () => {
     if(money >= 200000){
         money = money - 200000;
         cu2++;
-        cun2.innerText = "Vlastněno " + cu2 + "x";
+        cun2.innerText = "Vlastneno " + cu2 + "x";
         mp = mp+1250;
         mps.innerText = mp + " devizových korun za sekundu"
         Auto2 = setInterval(() => {
@@ -52,7 +67,7 @@ const buyAutoThree = () => {
     if(money >= 325000){
         money = money - 325000;
         cu3++;
-        cun3.innerText = "Vlastněno " + cu3 + "x";
+        cun3.innerText = "Vlastneno " + cu3 + "x";
         mp = mp+2000;
         mps.innerText = mp + " devizových korun za sekundu"
         Auto3 = setInterval(() => {
