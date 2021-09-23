279a4db39323356b0a4f3b0dfd9cae2d8728ca1c
MattiasJD <maty.demjan@seznam.cz>
2021-09-23 19:44:18 +0200

    v0.3

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ let btn = document.getElementById("btn");
 let counter = document.getElementById("counter");
 let autoOne = document.getElementById("autoOne");
 let autoTwo = document.getElementById("autoTwo");
+let autoThree = document.getElementById("autoThree")
 let cun1 = document.getElementById("cun1");
 let cun2 = document.getElementById("cun2");
+let cun3 = document.getElementById("cun3")
 let cu1 = 0;
 let cu2 = 0;
+let cu3 = 0;
 let mps = document.getElementById("mps");
 let mp = 0;
 const d = () => {
@@ -16,6 +19,7 @@ const d = () => {
 btn.onclick = d;
 let Auto;
 let Auto2;
+let Auto3;
 const buyAutoOne = () => {
     if(money >= 100000){
         money = money - 100000;
@@ -44,3 +48,17 @@ const buyAutoTwo = () => {
     }
 }
 autoTwo.onclick = buyAutoTwo;
+const buyAutoThree = () => {
+    if(money >= 325000){
+        money = money - 325000;
+        cu3++;
+        cun3.innerText = "Vlastněno " + cu3 + "x";
+        mp = mp+2000;
+        mps.innerText = mp + " devizových korun za sekundu"
+        Auto3 = setInterval(() => {
+            money = money + 2000;
+            counter.innerText = money + " devizových korun";
+        }, 1000);
+    }
+}
+autoThree.onclick = buyAutoThree;
