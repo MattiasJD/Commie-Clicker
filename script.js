2045e94523ad981c49486034d84e74484df40e65
MattiasJD <maty.demjan@seznam.cz>
2021-10-05 00:06:58 +0200

    v1.0

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ let cun1 = document.getElementById("cun1");
 let cun2 = document.getElementById("cun2");
 let cun3 = document.getElementById("cun3");
 let cun4 = document.getElementById("cun4");
+let cun5 = document.getElementById("cun5");
 let cun6 = document.getElementById("cun6");
 let cu1 = 0;
 let cu2 = 0;
@@ -20,19 +21,15 @@ let cu5 = 0;
 let cu6 = 0;
 let mps = document.getElementById("mps");
 let mp = 0;
-
+let close = document.getElementById("close");
+close.onclick=popup;
+function popup(){
+    document.getElementById("info").style.display = "none";
+}
+let mpc = 0;
 function d() {
-    if (cu4 >= 1) {
-        money = money + 3000;
+        money = money + (1000+mpc);
         counter.innerText = money + " devizových korun";
-    }if(cu5 >= 1){
-        money = money + 6000;
-        counter.innerText = money + " devizových korun";
-    }
-    else {
-        money = money + 1000;
-        counter.innerText = money + " devizových korun";
-    }
 }
 
 function buyBetterMouse1() {
@@ -40,6 +37,7 @@ function buyBetterMouse1() {
         money = money - 268000;
         cu4=cu4+1;
         cun4.innerText = "Vlastněno " + cu4 + "x";
+        mpc = mpc + 500;
     }
 }
 betterMouse1.onclick = buyBetterMouse1;
@@ -47,7 +45,8 @@ function buyBetterMouse2() {
     if (money >= 600000) {
         money = money - 600000;
         cu5=cu5+1;
-        cun4.innerText = "Vlastněno " + cu4 + "x";
+        cun5.innerText = "Vlastněno " + cu5 + "x";
+        mpc = mpc + 2000;
     }
 }
 betterMouse2.onclick = buyBetterMouse2;
@@ -57,7 +56,7 @@ function buyAutoOne() {
         money = money - 100000;
         cu1 = cu1 + 1;
         cun1.innerText = "Vlastněno " + cu1 + "x";
-        mp = mp + 500;
+        mp = mp + 200;
         mps.innerText = mp + " devizových korun za sekundu";
     }
 }
@@ -68,7 +67,7 @@ function buyAutoTwo() {
         money = money - 200000;
         cu2++;
         cun2.innerText = "Vlastněno " + cu2 + "x";
-        mp = mp + 1250;
+        mp = mp + 422;
         mps.innerText = mp + " devizových korun za sekundu"
     }
 }
@@ -79,7 +78,7 @@ function buyAutoThree() {
         money = money - 325000;
         cu3++;
         cun3.innerText = "Vlastněno " + cu3 + "x";
-        mp = mp + 2000;
+        mp = mp + 1000;
         mps.innerText = mp + " devizových korun za sekundu"
     }
 }
@@ -90,7 +89,7 @@ function buyAutoFour() {
         money = money - 680000;
         cu6++;
         cun6.innerText = "Vlastněno " + cu6 + "x";
-        mp = mp + 4500;
+        mp = mp + 2200;
         mps.innerText = mp + " devizových korun za sekundu"
     }
 }
@@ -102,7 +101,7 @@ function timer(){
 }
 setInterval(timer, 1000)
 
-let datum = new Date (1979, 0, 1,);
+let datum = new Date (1987, 0, 1,);
 let y = datum.getFullYear();
 let m = 1;
 let dd = datum.getDay();
@@ -112,12 +111,20 @@ function time(){
         dd = 1;
         m = m +1;
     }
-    if(m>=12){
+    if(m>=12&&dd>=30){
         m = 1;
         y = y +1;
     }
+    if( m === 12&& y === 1989 && dd === 10){
+        endgame();
+    }
     document.getElementById("date").innerText = dd +"."+ m +"."+ y;
 }
+function endgame (){
+    document.getElementById("end").style.display = "block";
+    document.getElementById("lovaky").innerHTML=money+" devizových korun.";
+}
+
 setInterval(time, 1000)
 
 btn.onclick = d;
